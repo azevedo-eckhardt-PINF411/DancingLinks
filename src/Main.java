5c0d46bd81527af80bfca1e23075a64171bf47bb
FranciscoRES <francisco.eckhardt@polytechnique.org>
2016-05-13 11:34:23 +0200

    Updated Main for testing

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,64 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
 
 public class Main {
 
 	static boolean debug=true;
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		if(debug){
+			FileInputStream is = null;
 			//read from tests/emc/sujet.txt
-			
-			DLX.solve("emc");
-			System.out.println("(Reponse attendue: 2)");
+			try{
+				is = new FileInputStream("tests/emc/sujet.txt");
+				System.setIn(is);
+				/*
+				Scanner in = new Scanner(System.in);
+				System.out.println(in.nextInt()+", "+in.nextInt());
+				in.close();*/
+				DLX.solve("emc");
+				System.out.println("(Reponse attendue: 2)");
+				is.close();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			
 			//read from tests/emc/knuth.txt
-			
-			DLX.solve("emc");
-			System.out.println("(Reponse attendue: ?");
+			try{
+				is = new FileInputStream(new File("tests/emc/knuth.txt"));
+				System.setIn(is);
+				DLX.solve("emc");
+				System.out.println("(Reponse attendue: ?)");
+				is.close();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			
 			//read from tests/pavage/scott.txt
-			
-			DLX.solve("pavage");
-			System.out.println("(Reponse attendue: ?");
+			try{
+				is = new FileInputStream(new File("tests/pavage/scott.txt"));
+				System.setIn(is);
+				DLX.solve("emc");
+				System.out.println("(Reponse attendue: ??)");
+				is.close();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		
 		else{
