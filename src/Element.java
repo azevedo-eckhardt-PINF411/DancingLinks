3cdce63e1a4d9db44e8e163ea44122d29abc737a
FranciscoRES <francisco.eckhardt@polytechnique.org>
2016-05-14 19:20:27 +0200

    Started Element and Header classes

---
+++ src/Element.java
@@ -0,0 +1,21 @@
+
+public abstract class Element {
+	Element left,right,up,down,column;
+	
+	public Element(Element l, Element r, Element u, Element d, Element c){
+		left=l;
+		right=r;
+		u=up;
+		d=down;
+		c=column;
+	}
+	
+	public Element getNextR(){
+		return right;
+	}
+	
+	public Element getNextD(){
+		return down;
+	}
+
+}
---
+++ src/Header.java
@@ -0,0 +1,14 @@
+
+public class Header extends Element {
+	int size;
+	char name;
+	public Header(Element l, Element r, Element u, Element d, Element c, int s, char n) {
+		super(l, r, u, d, c);
+		// TODO Auto-generated constructor stub
+		size=s;
+		name=n;
+	}
+
+
+
+}
