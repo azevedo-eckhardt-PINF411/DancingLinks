b3b52d5bc5c64d10403187f5c1cc10719bb40681
FranciscoRES <francisco.eckhardt@polytechnique.org>
2016-05-15 16:31:02 +0200

    Header constructor fix

--- src/Header.java
+++ src/Header.java
@@ -2,7 +2,7 @@
 public class Header extends Element {
 	int size;
 	int name;
-	public Header(Element l, Element r, Element u, Element d, Header c, int s, int n) {
+	public Header(Element l, Element r, Element u, Element d, Element c, int s, int n) {
 		super(l, r, u, d, c);
 		size=s;
 		name=n;
