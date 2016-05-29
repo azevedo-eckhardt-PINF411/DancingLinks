b2a8f0aa67136ca4f3dbd534f427dd0ab56b126b
Gabriel-Azevedo-Ferreira <az.fe.gabriel@gmail.com>
2016-05-29 19:09:10 +0200

    so aumentei os casos testados

--- src/Main.java
+++ src/Main.java
@@ -55,26 +55,22 @@ public class Main {
 				System.out.println("(Reponse attendue: ?)");
 				is.close();
 			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			
 			//read from tests/pavage/scott.txt
 			try{
-				is = new FileInputStream(new File("tests/pavage/scott.txt"));
+				is = new FileInputStream(new File("tests/pavage/mutilated_chessboard4.txt"));
 				System.setIn(is);
 				DLX p3=new DLX();
 				p3.solve("pavage");
 				System.out.println("(Reponse attendue: ??)");
 				is.close();
 			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 
@@ -82,36 +78,25 @@ public class Main {
 				is = new FileInputStream("tests/pavage/scott.txt");
 				System.setIn(is);
 				
-				Scanner in = new Scanner(System.in);
-
-
-				board b = new board(in);
-				b.print();
-
-				List<piece> pList = new ArrayList<piece>();
-				int listSize = in.nextInt();
-				int o = 0;
-				while(in.hasNext() && o < listSize){
-					pList.add(new piece(in));
-					pList.get(o++).print();
-					System.out.println();
-				}
-				
-				int [][] m = b.polyoToEMC(pList);
-				print(m);
-				
-				in.close();
-				is.close();
+				DLX p4 = new DLX();
+				p4.solve("pavage");
 				
 				
 			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			
+			/*try{
+				is = new FileInputStream("tests/pavage/tetris_5_8.txt");
+				System.setIn(is);
+				
+				DLX p5 = new DLX();
+				p5.solve("pavage");
+				
+				
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}*/
 			
 		}
 		
